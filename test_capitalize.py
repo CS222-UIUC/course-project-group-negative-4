a634523147e9c631df0723163b3ea5a5083ba74f
jaywoojo <jrjo2@illinois.edu>
2022-09-30 01:36:53 -0500

    pytest

--- test_capitalize.py
+++ test_capitalize.py
@@ -4,4 +4,7 @@ def hello(s):
     return s.capitalize()
 
 def test_capitalize_string():
-    assert capitalize_string('test') == 'Test'
+    assert hello('test') == 'Test'
+
+def test_capitalize_string_2():
+    assert hello('tests') == 'Tests'
