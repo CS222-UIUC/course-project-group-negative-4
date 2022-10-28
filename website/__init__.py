a579c3fb846fd36390dff83400180005e29690c9
Bryan Finn <bryan.finn8@gmail.com>
2022-10-28 17:14:20 -0500

    transitioned current bootstrap to flask and reorganizing the file structure

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from os import path
+
+
+def create_app():
+    # initilize
+    app = Flask(__name__)
+
+    from .views import views
+    
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
